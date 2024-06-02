628af63cdef5a77bd487f6bf92ebdb1e0a200358
Alaka A J <alaka03aj@gmail.com>
2024-06-02 23:35:06 +0530

    frontend: autolayout component

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,12 +1,56 @@
 const { widget } = figma
-const { AutoLayout} = widget
+const { AutoLayout, Text, Input, useSyncedState } = widget
+
+type VerticalHorizontalPadding = {
+  vertical?: number
+  horizontal?: number
+}
 
 const Timer = () => {
-  return (
-    <AutoLayout fill="#FFFFFF">
+  const lightMode = {
+    background: "#FFFFFF",
+    button: "#000000",
+    text: "#000000",
+    buttonText: "#FFFFFF"
+  }
 
-      <AutoLayout direction="horizontal">
+  const darkMode = {
+    background: "#000000",
+    text: "#FFFFFF",
+    button: "#FFFFFF"
+  }
+
+  const font = "Space Mono"
+
+  const padding: VerticalHorizontalPadding = { vertical: 45, horizontal: 70 }
+
+  const [seconds, setSeconds] = useSyncedState("seconds", "0")
+
+  return (
+    <AutoLayout fill={lightMode.background} width={400} height={200} padding={padding} spacing={10}>
+
+      <AutoLayout direction="horizontal" spacing={8}>
         {/* for taking input & buttons */}
+        <Input
+          value={seconds}
+          placeholder="Enter time in seconds"
+          onTextEditEnd={(e) => {
+            setSeconds(e.characters);
+          }}
+          fontSize={12}
+          fill={lightMode.text}
+          width={167}
+          inputFrameProps={{
+            fill: lightMode.background,
+            stroke: lightMode.text,
+            cornerRadius: 3,
+            padding: 7.5,
+          }}
+          inputBehavior="wrap"
+        />
+        <AutoLayout width={76} height={30} fill={lightMode.button} padding={{ vertical: 6, horizontal: 22 }} cornerRadius={3} spacing={"auto"}>
+          <Text fontFamily={font} fill={lightMode.buttonText} fontSize={12}>START</Text>
+        </AutoLayout>
       </AutoLayout>
 
       <AutoLayout direction="horizontal">
